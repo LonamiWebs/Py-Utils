024d64b0090ea7b517617e42eb7818247ecab87b
Lonami Exo <totufals@hotmail.com>
2017-03-14 17:15:59 +0100

    [interpolation] Allow passing lists of operators to extrapolate

--- interpolation/extrapolation_generic.py
+++ interpolation/extrapolation_generic.py
@@ -22,21 +22,29 @@ def get_subseq(s, op):
     return [op(s[i-1], s[i]) for i in range(1, len(s))]
 
 
-def get_subseqs(s, op):
-    """Returns a list of sequences given when applying (op)
+def get_subseqs(s, ops):
+    """Returns a list of sequences given when applying the list of (ops)
        on them, until a constant one is found, thus:
-       new[0] = next seq of s with op
-       new[i] = next seq of new[i-1] with op"""
+       new[0] = next seq of s with ops[0]
+       new[i] = next seq of new[i-1] with op[i]
+       
+       If 'ops' is not a list, then the same operation will be repeated.
+       The length of 'ops' should be equal to the length of 's' minus 1"""
     if len(s) < 2:
         # We can't get the next sequence based on two terms if there's only one
         return []
     
+    if not isinstance(ops, list):
+        ops = [ops for _ in range(len(s)-1)]
+    
     # Start with the initial subsequence
-    subseqs = [get_subseq(s, op)]
+    subseqs = [get_subseq(s, ops[0])]
     
     # And base the next subsequences on the previous one until they're constant
+    i = 1
     while not is_constant(subseqs[-1]) and len(subseqs[-1]) > 1:
-        subseqs.append(get_subseq(subseqs[-1], op))
+        subseqs.append(get_subseq(subseqs[-1], ops[i]))
+        i += 1
     
     return subseqs
 
@@ -61,24 +69,30 @@ def printtree(s, ss):
         print(' '.join([str(n).center(pad) for n in ss[i]]))
 
 
-def nextitem(s, op, invop):
+def nextitem(s, ops, invops):
     """Tries to guess the next item on the sequence 's' by using a table
-       of (op). This method will fail if the sequence doesn't provide enough
+       of (ops). This method will fail if the sequence doesn't provide enough
        information, and will assume that the latest sequence should remain
        constant.
        
-       The inverse operation 'invop' is also required to transverse back
+       The inverse operations 'invops' are also required to transverse back
        the tree in order to get the proper results"""
-    ds = get_subseqs(s, op)
+    ss = get_subseqs(s, ops)
     # We assume the last item all below Repeats
-    r = ds[-1][-1]
+    r = ss[-1][-1]
+    
+    # Ensure we have a list of inverse operations
+    if not isinstance(invops, list):
+        invops = [invops for _ in range(len(s)-1)]
     
     # We need to chain this result to the previous ones
-    for i in range(len(ds)-2, -1, -1):
-        r = invop(ds[i][-1], r)
+    # Range of invops: end....1
+    # Range of subseq: end-1..0
+    for i in range(len(ss)-1, 0, -1):
+        r = invops[i](ss[i-1][-1], r)
     
     # Now we have the accumulated inverse operation
-    return invop(s[-1], r)
+    return invops[0](s[-1], r)
 
 
 if __name__ == '__main__':
@@ -113,6 +127,25 @@ if __name__ == '__main__':
     for i in range(2):
         seq.append(nextitem(seq, div, divi))
         subseq = get_subseqs(seq, div)
+        
+        printtree(seq, subseq)
+        print()
+    
+    print('-' * 80)
+    print()
+    
+    seq = [1, 2, 6, 24]
+    # Using division, then difference
+    ops = [div, diff]
+    invops = [divi, diffi]
+    
+    subseq = get_subseqs(seq, ops)
+    printtree(seq, subseq)
+    
+    print(f'\nNext 2 items of {seq}:')
+    for i in range(2):
+        seq.append(nextitem(seq, ops, invops))
+        subseq = get_subseqs(seq, ops)
         
         printtree(seq, subseq)
         print()
