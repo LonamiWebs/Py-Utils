22b2d22d106fe15ce06aab5f8888a7c9573c5ab3
Marcos Rosa <marcos.cantor@gmail.com>
2017-10-24 10:47:06 -0200

    add functions to translate minutes in hours/days/seconds

---
+++ business/date.py
@@ -0,0 +1,11 @@
+def minutes_in_hours(minutes):
+    """Translate minutes in hours"""
+    return minutes / 60
+
+def minutes_in_seconds(minutes):
+    """Translate minutes in seconds"""
+    return minutes * 60
+
+def minutes_in_days(minutes):
+    """Translate minutes in days"""
+    return minutes / 1440
